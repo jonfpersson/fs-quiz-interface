06cc34e0098ee31b503f30d4434294a475e343db
jonfpersson <jonfpersson@gmail.com>
2023-10-07 16:13:04 +0200

    Add script

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# fs-quiz-interface
+# Quiz API Interface
+
+This repository contains a Python program that interfaces with the "https://api.fs-quiz.eu/" API to retrieve questions and answers. The program allows you to access and display quiz questions along with their corresponding answers.
+
+## Prerequisites
+
+Before using this program, make sure you have the following prerequisites installed on your system:
+
+- Python (>=3.6)
+- The `requests` library for making HTTP requests. You can install it using `pip`:
+pip install requests
+
+
+## Getting Started
+
+Follow these steps to get started with the Quiz API Interface:
+
+1. Clone this repository to your local machine:
+
+2. Navigate to the repository directory:
+
+3. Open the `quiz.py` file in your preferred code editor.
+
+4. Replace `'YOUR_API_KEY'` with your actual API key.
+
+## Usage
+
+To use the program to retrieve quiz questions and answers, follow these steps:
+
+1. Run the Python script:
+
+2. The program will send an HTTP GET request to the "https://api.fs-quiz.eu/1/{api_key}/quiz" endpoint, where `{api_key}` is your API key.
+
+3. The program will display the quiz questions and answers in the console.
+
+
+## Troubleshooting
+
+- If you encounter any issues with the program or receive errors, make sure you have correctly replaced `'YOUR_API_KEY'` with your actual API key.
+
+- Ensure that you have a stable internet connection to access the API.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
+
+## Acknowledgments
+
+- Special thanks to the "https://api.fs-quiz.eu/" API for providing the quiz data.
+
+## Contact
+
+If you have any questions or need further assistance, feel free to contact me.
---
+++ quiz.py
@@ -0,0 +1,79 @@
+import requests
+import json
+
+def print_correct_answer(response):
+    # Load the JSON response
+    response_data = json.loads(response)
+
+    # Get the correct answer
+    correct_answer = None
+    for answer in response_data["answers"]:
+        if answer["is_correct"]:
+            correct_answer = answer["answer_text"]
+            break
+
+    if correct_answer:
+        print(f"Correct Answer: {correct_answer}")
+    else:
+        print("No correct answer found.")
+
+
+
+def send_request(extension):
+    # Define the URL with the API key
+    url = f"https://api.fs-quiz.eu/1/YOUR-API-KEY/{extension}"
+    print(url)
+
+    try:
+        # Send the GET request
+        headers = {'User-Agent': 'YourApp/1.0'}
+        response = requests.get(url, headers=headers)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # If successful, print the response content
+            return response.text
+        else:
+            print(f"Error: Status code {response.status_code}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+
+def display_menu():
+    print("")
+    print("Welcome to the Start Menu")
+    print("1. Get quiz answer")
+    print("2. Get random question")
+    print("3. Option 3")
+    print("4. Option 4")
+    print("5. Exit")
+
+while True:
+    display_menu()
+    choice = input("Enter your choice (1-5): ")
+
+    if choice == '1':
+        
+        id = input("Enter question id ")
+        print_correct_answer(send_request("question/" + id))
+        
+    elif choice == '2':
+        id = str(random.randint(1, 1000))
+
+        response = send_request("question/" + id + "/info")
+        parsed = json.loads(response)
+        print(parsed["text"])
+
+    elif choice == '3':
+        print("You selected Option 3")
+        # Add code for Option 3 here
+    elif choice == '4':
+        print("You selected Option 4")
+        # Add code for Option 4 here
+    elif choice == '5':
+        break
+    else:
+        print("Invalid choice. Please choose a number between 1 and 5.")
+
+# Continue with the rest of your script after the menu
